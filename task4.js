daed96e778507e06961a0b94b54eddcbfa6de6c3
rashedrk <rashedrk001@gmail.com>
2023-07-27 23:36:54 +0600

    task 4

--- task4.js
+++ task4.js
@@ -1,7 +1,6 @@
 // Task 4: Create a function that takes a sorted array of numbers and a target value as input. The function should find two numbers in the array that add up to the target value. Return an array containing the indices of the two numbers.
 
 const findPair = (arr, target) => {
-
     for (let i = 0; i < arr.length; i++) {
             let sum = arr[i] + arr[i+1];
             if(sum === target)
